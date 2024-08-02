2867d0a54c9a1dcb7948481ebd11c446c76e56d5
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-08-02 22:08:26 +0530

    fuck

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,15 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { Router } from 'react-router-dom'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-=======
-const clientId = "243658490429-188fcjqrq0co0i389g4bpmn31rlkpfkf.apps.googleusercontent.com"
->>>>>>> c835b85 (second commit:)
-=======
 
->>>>>>> f2e3b8e (final commit)
 
-=======
->>>>>>> d274ee3 (first commit)
+
+
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <GoogleOAuthProvider clientId={clientId}>
